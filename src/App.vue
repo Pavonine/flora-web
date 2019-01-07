4fac066735f79e70f3b6da816152f1587cc4ba6f
buoyantair <buoyantair@protonmail.com>
2019-01-07 19:29:41 +0530

    Add basic Header structure

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,31 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
+    <!-- <div id="nav">
+      <router-link to="/">Home</router-link>|
       <router-link to="/about">About</router-link>
-    </div>
+    </div>-->
     <router-view/>
   </div>
 </template>
 
 <style lang="scss">
+// CSS Reset
+@import './partials/reset';
+
+// Ubuntu Font
+@import url('https://fonts.googleapis.com/css?family=Ubuntu:300,400,500,700');
+
+body,
+html {
+  margin: 0;
+  padding: 0;
+  min-width: 300px;
+}
+
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  background: white;
+  display: grid;
+  font-family: 'Ubuntu', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
---
+++ src/components/Header.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="header">
+    <section class="app-heading">
+      <h1>{{ appName }}</h1>
+    </section>
+    <section class="app-settings">
+      <button class="settings-button">Button</button>
+    </section>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component
+export default class Header extends Vue {
+  @Prop() private appName!: string;
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+.header {
+  display: grid;
+  background: white;
+  box-shadow: 0px 1px 50px rgba(0, 0, 0, 0.1);
+  grid-template-columns: 0.9fr 0.1fr;
+  padding: 0 20px;
+  height: 60px;
+  .app-heading {
+    grid-column: 1;
+    display: flex;
+    flex-flow: column;
+    justify-content: center;
+    h1 {
+      text-align: left;
+      font-weight: 500;
+      font-size: 20px;
+    }
+  }
+
+  .app-settings {
+    grid-column: 2;
+    display: flex;
+    flex-flow: column;
+    justify-content: center;
+    align-items: center;
+    .settings-button {
+      height: 40px;
+      width: 40px;
+    }
+  }
+}
+</style>
